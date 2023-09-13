3a1880d156d9daf316a00370c64600df47fd4be8
EC2 Default User <143916489+jimmy96224@users.noreply.github.com>
2023-09-13 10:18:48 +0000

    [wip]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,38 +1,44 @@
 class BooksController < ApplicationController
   def new
   end
-  
+
   def create
     book = Book.new(book_params)
     book.save
-    # 投稿内容表示
-    redirect_to '/books'
-    
+    redirect_to book_path(book.id)
   end
-  
+
   def index
     @book = Book.new
     @books = Book.all
   end
-  
 
-  
+
+
   def show
     @book = Book.find(params[:id])
-  end
-  
 
-  
+  end
+
+
+
   def edit
-  end  
-  
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
   end
-  
-  
+
+
   private
   def book_params
     params.require(:book).permit(:title, :body)
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
   root :to => 'homes#top'
-  get '/books' => 'books#index'
-
-  post '/books' =>'books#create'
-
-  # 投稿成功不要？
-  # get '/books/174350' =>
-
-  # edit
-  get '/books/174427/edit' => 'books#edit'
-  # show
-  get '/books/174420' => 'books#show'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
-  # resources :books
+  resources :books
 end
