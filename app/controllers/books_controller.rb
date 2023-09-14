34c172068679527eedc21717db009e21a2b7f0ff
EC2 Default User <143916489+jimmy96224@users.noreply.github.com>
2023-09-14 05:23:52 +0000

    wip

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,14 +3,13 @@ class BooksController < ApplicationController
   end
 
   def create
-    book = Book.new(book_params)
-    if book.save
+    @book = Book.new(book_params)
+    if @book.save
     flash[:notice] = "Book was successfully created."
     redirect_to book_path(book.id)
     else
     @books = Book.all
-    @book = Book.new
-    render :index, status: :unprocessable_entity
+    render :index
     end
   end
 
@@ -29,12 +28,11 @@ class BooksController < ApplicationController
   end
 
   def update
-    book = Book.find(params[:id])
-    if book.update(book_params)
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
     flash[:notice] = "Book was successfully updated."
-    redirect_to book_path(book.id)
+    redirect_to book_path(@book.id)
     else
-    @book = Book.new
     @books = Book.all
     render :edit
     end
